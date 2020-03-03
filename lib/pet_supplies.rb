1a77b8b08f3310d40f8093f04fb47b340a00f515
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-03 14:35:30 +0000

    first commit

---
+++ lib/pet_supplies.rb
@@ -0,0 +1,6 @@
+require "pet_supplies/version"
+
+module PetSupplies
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pet_supplies/version.rb
@@ -0,0 +1,3 @@
+module PetSupplies
+  VERSION = "0.1.0"
+end
