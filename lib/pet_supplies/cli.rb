34be249affaef69f02167c8c6933abf903197934
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-03 15:14:13 +0000

    pseudo code 1

--- lib/pet_supplies/cli.rb
+++ lib/pet_supplies/cli.rb
@@ -1,18 +1,24 @@
 class PetSupplies::CLI
   
   def start 
-    puts "Welcome to Pet Supplies! Your stop for your pets favorite food, treats, and apparel"
+    puts "Welcome to Pet Supplies! Your stop for your pets favorite food, treats, and toys."
     puts "Which pet would you like more information about?"
-    puts "cats or dogs
-    puts ""
-    input = gets.strip
+    puts "Type either dogs, cats, or small animals?"
+    
+    input = gets.strip.downcase
+    
     case input
-    when
-      puts
-      when
-        puts
-        when
-          puts
+      when "dogs"
+        puts "doggies"
+      when "cats"
+        puts ""
+      when "small_animals"
+        puts ""
+      when "exit" 
+        puts "Goodbye"
+      else 
+        puts "invalid choice"
+     end
           
   end
   
